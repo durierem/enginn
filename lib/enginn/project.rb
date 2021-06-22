136de181100c495b91e1a62f0e959deea39c6a4f
Rémi Durieu <mail@remidurieu.dev>
2021-06-22 23:29:01 +0200

    Add Project.patch

--- lib/enginn/project.rb
+++ lib/enginn/project.rb
@@ -5,5 +5,9 @@ require_relative 'resource'
 module Enginn
   class Project < Resource
     resource 'project', only: :get
+
+    def self.patch(attributes)
+      Enginn.request(:patch, 'project', attributes: attributes)
+    end
   end
 end
